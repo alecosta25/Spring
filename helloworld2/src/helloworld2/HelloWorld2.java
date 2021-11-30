61081fa4450f180186c57235746ead734ca144df
alecosta25 <alesantoscosta95@gmail.com>
2021-11-30 20:33:35 -0300

    projeto hello world 2

---
+++ helloworld2/src/helloworld2/HelloWorld2.java
@@ -0,0 +1,10 @@
+package helloworld2;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("minhas metas esta semana é dar andamento no blog e no integrador e me focar mais no meu desempenho em classe");
+
+	}
+
+}
